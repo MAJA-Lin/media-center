730763669e94f3618981e2946302a04885d08c13
MAJA-Lin <typhoon31815@gmail.com>
2015-06-26 16:34:47 +0800

    Add multidimensional array verification into book_comment.php

--- book_comment.php
+++ book_comment.php
@@ -32,7 +32,10 @@
 	$_COOKIE['comment'] = $array_comment;
 
 
-
+	/*
+	*The subfuncion which can show the book comment.
+	*
+	*/
 
 	Function book_comment_detail($comment) {
 
@@ -40,11 +43,31 @@
 
 		if(isset($comment)) {
 
-			while( $i < count($comment) ) {
-				echo "Score: ".$comment[$i]['score']."<br>Comment: ".$comment[$i]['comment']."<br>";
-				$i++;
+			/*
+			*To verify if the array is multidimensional or not.
+			*/
+			if( count($comment) == count($comment, COUNT_RECURSIVE)) {
+				/*
+				*If the array is not multidimensional, then do the print here.
+				*And check SID to print "No Comment".
+				*/
+				if($comment['SID'] == null) {
+					echo "<h2>No comment. How about add some comment?</h2>";
+				}
+				else {
+					echo "Score: ".$comment['score']."<br>Comment: ".$comment['comment']."<br>";
+				}
 			}
 
+			/*
+			*If the array is multidimensional, do normally print and the counter $i will work.
+			*/
+			else {
+				while( $i < count($comment) ) {
+					echo "Score: ".$comment[$i]['score']."<br>Comment: ".$comment[$i]['comment']."<br>";
+					$i++;
+				}
+			}
 		}
 		else {
 			echo "<h2>No comment. How about add some comment?</h2>";
